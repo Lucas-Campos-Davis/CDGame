567d2bbd7eae2fda57bc283aed9ecf96916df4d8
Gabriel Campos-Davis <gcamposdavis@gmail.com>
2020-09-15 14:36:21 -0700

    fixed health and bullet system
    made it actually work this time

--- rogue/Scenes/KinematicBody2D.gd
+++ rogue/Scenes/KinematicBody2D.gd
@@ -1,16 +1,17 @@
 extends KinematicBody2D
 
-var speed = 100
+var speed = 50
 var velocity = Vector2()
 
 func start(pos, dir):
 	position = pos
 	velocity = Vector2(speed, 0).rotated(dir)
-	get_node("Bullet2").rotation = dir
+	get_node("bulletTexture").rotation = dir
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
+		get_parent().get_node(collision.collider.name).hurt(1)
 		queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
--- rogue/Scenes/StaticBody2D.gd
+++ rogue/Scenes/StaticBody2D.gd
@@ -1,22 +1,19 @@
 extends StaticBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 var Bullet = preload("res://Scenes/Bullet.tscn")
 var wait = 0;
 onready var Muzzle = get_node("Muzzle")
-# Called when the node enters the scene tree for the first time.
+
+func shoot():
+	var b = Bullet.instance()
+	b.start(Muzzle.position, 3)
+	get_parent().add_child(b)
+
 func _process(delta):
 	if wait == 0:
 		shoot()
 		wait = 30
 	else:
 		wait -= 1
-func shoot():
-	# "Muzzle" is a Position2D placed at the barrel of the gun.
-	var b = Bullet.instance()
-	b.start(Muzzle.position, 90)
-	print("made bullet")
-	get_parent().add_child(b)
