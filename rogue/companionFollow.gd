92542a76f0fd138f257d40b14d354a4a8b69f36c
Gabriella Campos-Davis <gcamposdavis@gmail.com>
2024-01-07 22:40:20 -0800

    made companion follow player without lagging behind

--- rogue/companionFollow.gd
+++ rogue/companionFollow.gd
@@ -1,35 +1,40 @@
 extends CharacterBody2D
 
 var player;
+var playerBody
 var companionDirection;
+var companionDirectionCoords;
 var travelCoords;
+
 
 func _ready():
 	velocity = Vector2.ZERO
+	player = get_parent().find_child("player_animations");
+	playerBody = get_parent().find_child("player");
 	
 func go_to_position(coords: Vector2):
-	velocity = Vector2.ZERO
-	velocity += (global_position.direction_to(coords))*40;
-	
+	velocity = Vector2.ZERO;
+	velocity += (global_position.direction_to(coords)) * (40);
+	if playerBody.get_velocity() != Vector2.ZERO:
+		velocity += (global_position.direction_to(coords)) * (20);
 	
 func _physics_process(delta):
-	if Input.is_action_just_released("click"):
-		go_to_position(get_global_mouse_position());
+	velocity = Vector2.ZERO;
+	
+	companionDirection = player.companionDirection;
+	#these are assigned counter-intuitivley,
+	#but it's so the companion will not be n front of the player and 'block' their view
+	if companionDirection == "LEFT": 
+		companionDirectionCoords = get_parent().find_child("friend_spot_right").global_position;
+	if companionDirection == "RIGHT":
+		companionDirectionCoords = get_parent().find_child("friend_spot_left").global_position;
 		
-	else:
-		player = get_parent().find_child("player_animations");
-		companionDirection = player.companionDirection;
-		if companionDirection == "LEFT":
-			travelCoords = get_parent().find_child("friend_spot_right").global_position;
-		if companionDirection == "RIGHT":
-			travelCoords = get_parent().find_child("friend_spot_left").global_position;
-			
-		
-
-		if global_position.distance_to(travelCoords) < 1:
+	travelCoords = companionDirectionCoords;
+	
+	if global_position.distance_to(travelCoords) < 1:
 			velocity = Vector2.ZERO;
-		else:
-			go_to_position(travelCoords);
+	else:
+		go_to_position(travelCoords);
 			
 	move_and_slide();
 	
