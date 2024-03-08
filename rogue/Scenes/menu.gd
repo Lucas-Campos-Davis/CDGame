fec81433a93b3181a30d75c6f30844aec6e6a202
Gabriella Campos-Davis <gcamposdavis@gmail.com>
2024-03-07 22:09:51 -0800

    paper birch

--- rogue/Scenes/menu.gd
+++ rogue/Scenes/menu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://forestScene.tscn");
+	get_tree().change_scene_to_file("res://real_forest.tscn");
 
 func _on_options_pressed():
 	pass # Replace with function body.
