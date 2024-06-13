c1f4cceff218d5d380386fd22745fb69f9df0704
Gabriella Campos-Davis <gcamposdavis@gmail.com>
2024-06-12 23:49:45 -0700

    basic camera point of interest system

---
+++ rogue/Scenes/forest_parts/gameCamera.gd
@@ -0,0 +1,31 @@
+extends Camera2D
+
+@onready var camera = %gameCamera;
+
+@onready var player = %player;
+@onready var points = %points_of_interest.get_children();
+
+@onready var cameraTarget = player;
+var isCloseToPoint = false;
+var newTarget = false;
+var t = 0.0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	for n in range(points.size()):
+		if player.global_position.distance_to(points[n].global_position) < 85:
+			if cameraTarget.name != points[n].name:
+				camera.global_position = points[n].global_position;
+				camera.align();
+			camera.global_position = cameraTarget.global_position;
+	if cameraTarget.name == "player":
+		camera.global_position = player.global_position;
+		camera.align();
+	
+
+
+
